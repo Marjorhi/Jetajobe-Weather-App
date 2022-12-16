eb81936547eda5cb3f4a320a9dc024f4597a1fbb
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-16 17:57:33 +0800

    Adding help page

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 
 const app = express()
 
+//app.com
 app.get('', (req, res) => { //Customize what we're going to send back to the requester
     res.send('Hi express!')
 })
+
+//app.com/help
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
