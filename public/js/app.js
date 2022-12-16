fbec7e09f5dc7064bfd5999c51c11503e0894b5c
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-17 07:20:14 +0800

    Creating message1 variable

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,14 @@
-fetch(`http://localhost:3000/weather?cityName=`).then((response) => {
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
+weatherForm.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    const location = search.value
+
+    fetch(`http://localhost:3000/weather?cityName=` + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             console.log(data.error)
@@ -8,14 +18,4 @@ fetch(`http://localhost:3000/weather?cityName=`).then((response) => {
         }
     })
 })
-
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-
-weatherForm.addEventListener('submit', (e) => {
-
-    e.preventDefault()
-    const location = search.value
-    
-    console.log(location)
 })
