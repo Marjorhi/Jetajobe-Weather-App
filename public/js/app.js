7893581929785a6c02f7e5bfd6d94f2e6e005efe
Marjorhi <marjoriejetajobe0524@gmail.com>
2022-12-17 16:50:49 +0800

    Updating files

--- public/js/app.js
+++ public/js/app.js
@@ -3,21 +3,24 @@ const search = document.querySelector('input')
 const message1 = document.querySelector('#message1')
 const message2 = document.querySelector('#message2')
 
-message1.textContent = 'Loading...'
 
 weatherForm.addEventListener('submit', (e) => {
 
     e.preventDefault()
     const location = search.value
 
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
     fetch(`http://localhost:3000/weather?cityName=` + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
-            console.log(data.error)
+            message1.textContent = data.error //If error, render error
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+                message1.textContent = data.location
+                message2.textContent = data.forecast
+                search.value = ''
+        } //If no error, render location and forecast
     })
 })
 })
